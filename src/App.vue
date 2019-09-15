c61837f85a5f1038a2958192bab89632df061095
qiannian18 <1434222222@qq.com>
2019-09-16 01:20:48 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<b-container fluid>
+			<myheader></myheader>
+		</b-container>
+		<b-container fluid>
+			<router-view></router-view>
+		</b-container>
+		<b-container fluid>
+			<router-view></router-view>
+		</b-container>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import myheader from "./components/header.vue"
+	export default {
+		name: 'app',
+		components: {
+			myheader,
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import BootstrapVue from 'bootstrap-vue'
+import router from "./routers/router.js"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
+	router,
   render: h => h(App),
 }).$mount('#app')
