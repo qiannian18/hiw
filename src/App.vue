99340ac03f866e521575575e0aaed748aaa68656
qiannian18 <1434222222@qq.com>
2019-09-17 00:22:17 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,25 @@
 <template>
 	<div id="app">
-<<<<<<< HEAD
-		
-=======
-		<b-container fluid style="margin: 0;padding: 0;">
-			<myheader></myheader>
-		</b-container>
-		<b-container fluid style="margin: 0;padding: 0;height: 21.875rem;">
-			<mycarousel></mycarousel>
-		</b-container>
-		<b-container fluid>
-			<router-view></router-view>
-		</b-container>
->>>>>>> 39616ae008359a05037fd5e9bc6592b2cd9fe45d
+		<Row>
+			<Col span="24">
+				<div style="width: 100%;width: 3.125rem;background: red;">
+					
+				</div>
+			</Col>
+		</Row>
 	</div>
 </template>
 
 <script>
-<<<<<<< HEAD
-	
 	export default {
 		name: 'app',
 		components: {
-			
-=======
-	import {mycontent,myheader,mycarousel} from "./components"
 
-	export default {
-		name: 'app',
-		components: {
-			mycontent,
-			myheader,
-			mycarousel
->>>>>>> 39616ae008359a05037fd5e9bc6592b2cd9fe45d
 		}
 	}
 </script>
 
-<style scoped="scoped">
+<style scoped>
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-
 import router from "./routers/router.js"
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 Vue.use(iView);
 Vue.config.productionTip = false
-
-=======
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import router from "./routers/router.js"
-
-Vue.config.productionTip = false
-Vue.use(ElementUI)
->>>>>>> 39616ae008359a05037fd5e9bc6592b2cd9fe45d
-
 new Vue({
 	router,
   render: h => h(App),
