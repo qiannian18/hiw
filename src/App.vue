bc7de209f5d45014d4714ce3d229aeb76210c027
qiannian18 <1434222222@qq.com>
2019-09-17 00:32:43 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,23 @@
 	<div id="app">
 		<Row>
 			<Col span="24">
-				<div style="width: 100%;width: 3.125rem;background: red;">
+				 <Affix>
+				<div style="width: 100%;height: 5.625rem;background:rgba(255,255,255,.25);">
+					
+				</div>
+				</Affix>
+			</Col>
+		</Row>
+		<Row>
+			<Col span="24">
+				<div style="width: 100%;height: 29.375rem;background: #42B983;">
+					
+				</div>
+			</Col>
+		</Row>
+		<Row>
+			<Col span="24">
+				<div style="width: 100%;height: 129.375rem;">
 					
 				</div>
 			</Col>
