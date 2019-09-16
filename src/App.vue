b11f222c9eab6dea2cdf836cce121a49ccd88bc8
qiannian18 <1434222222@qq.com>
2019-09-17 00:37:08 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,25 @@
 	<div id="app">
 		<Row>
 			<Col span="24">
-				 <Affix>
-				<div style="width: 100%;height: 5.625rem;background:rgba(255,255,255,.25);">
-					
+			<Affix @on-change="change">
+				<div style="width: 100%;height: 5.625rem;background:rgba(255,255,255,.25);position: fixed;z-index: 999;">
+
 				</div>
-				</Affix>
+			</Affix>
 			</Col>
 		</Row>
 		<Row>
 			<Col span="24">
-				<div style="width: 100%;height: 29.375rem;background: #42B983;">
-					
-				</div>
+			<div style="width: 100%;height: 29.375rem;background: #42B983;">
+
+			</div>
 			</Col>
 		</Row>
 		<Row>
 			<Col span="24">
-				<div style="width: 100%;height: 129.375rem;">
-					
-				</div>
+			<div style="width: 100%;height: 129.375rem;">
+
+			</div>
 			</Col>
 		</Row>
 	</div>
@@ -31,6 +31,11 @@
 		name: 'app',
 		components: {
 
+		},
+		methods:{
+			change(status){
+                this.$Message.info(`Status: ${status}`);
+            }
 		}
 	}
 </script>
