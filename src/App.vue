29c0ee062ade0e71340020efb0e18953ee3793c1
qiannian18 <44380801@qq.com>
2019-09-17 16:23:01 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,125 @@
 <template>
 	<div id="app">
-		<Row>
-			<Col span="24">
-			<Affix @on-change="change">
-				<div style="width: 100%;height: 5.625rem;background:rgba(255,255,255,.25);position: fixed;z-index: 999;">
+		<b-container fluid style="padding: 0;margin: 0;">
+			<b-navbar toggleable="lg" class="navbar_header" fixed="top">
+				<b-navbar-brand href="#">NavBar</b-navbar-brand>
 
-				</div>
-			</Affix>
-			</Col>
-		</Row>
-		<Row>
-			<Col span="24">
-			<div style="width: 100%;height: 29.375rem;background: #42B983;">
+				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-			</div>
-			</Col>
-		</Row>
-		<Row>
-			<Col span="24">
-			<div style="width: 100%;height: 129.375rem;">
+				<b-collapse id="nav-collapse" is-nav>
+					<b-navbar-nav style="margin: auto;" class="search_nav">
+							<input type="text" class="serch_css"/>
+							<button type="button" class="serch_btn">搜索</button>	
+					</b-navbar-nav>
+					
+				</b-collapse>
+			</b-navbar>
+		</b-container>
+		<!-- 轮播部分 -->
+		<b-container fluid style="margin: 0;padding: 0;">
 
-			</div>
-			</Col>
-		</Row>
+			<Carousel v-model="value3" :autoplay="setting.autoplay" :autoplay-speed="setting.autoplaySpeed" :dots="setting.dots"
+			 :radius-dot="setting.radiusDot" :trigger="setting.trigger" :arrow="setting.arrow" :height="setting.height">
+				<CarouselItem>
+					<div class="demo-carousel" style="height: 100%;">
+						<img src="https://picsum.photos/1024/480/?image=52" style="width: 100%;height: 100%;"/>
+					</div>
+				</CarouselItem>
+				<CarouselItem>
+					<div class="demo-carousel" style="height: 100%;">
+						<img src="https://picsum.photos/1024/480/?image=52" style="width: 100%;height: 100%;"/>
+					</div>
+				</CarouselItem>
+				<CarouselItem>
+					<div class="demo-carousel" style="height: 100%;">
+						<img src="https://picsum.photos/1024/480/?image=52" style="width: 100%;height: 100%;"/>
+					</div>
+				</CarouselItem>
+				<CarouselItem>
+					<div class="demo-carousel" style="height: 100%;">
+						<img src="https://picsum.photos/1024/480/?image=52" style="width: 100%;height: 100%;"/>
+					</div>
+				</CarouselItem>
+			</Carousel>
+
+		</b-container>
+		
+		<b-container fluid style="background: #004085;height: 81.25rem">
+			
+		</b-container>
+
+
 	</div>
 </template>
 
 <script>
+	import './css/icon.css'
 	export default {
 		name: 'app',
 		components: {
 
 		},
-		methods:{
-			change(status){
-                this.$Message.info(`Status: ${status}`);
-            }
-		}
+		data() {
+			return {
+				value3: 0,
+				setting: {
+					autoplay: false,
+					autoplaySpeed: 2000,
+					dots: 'inside',
+					radiusDot: true,
+					trigger: 'click',
+					arrow: 'hover',
+					height: 300
+				}
+			}
+		},
 	}
 </script>
 
-<style scoped>
-
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	b-container{
+		margin: 0;
+		padding: 0;
+	}
+</style>
+<style scoped="scoped">
+	.navbar_header {
+		background: rgba(255, 255, 255, .25);
+	}
+	@media screen and (min-width: 765px){
+		.serch_css{
+				width: 30.55rem;
+		}
+	}
+	.serch_css{
+		height: 2.8rem;
+		border: 0rem;
+		padding-left: 0.3125rem;
+		background: rgba(255, 255, 255, .30);
+		outline:none;
+		border-radius: 0.625rem;
+	}
+	.search_nav{
+		position: relative;
+	}
+	.search_nav>button{
+		 position: absolute;
+		    right:2%;
+		    top:18%;
+	}
+	.serch_btn{
+		color: #fff;
+		font-size: 0.9375rem;
+		background: linear-gradient(to right,#B23AEE , #AB82FF);
+		border: 0rem;
+		height: 1.975rem;
+		width: 3.525rem;
+		outline:none;
+		border-radius: 0.525rem;
+	}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./routers/router.js"
+
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(iView);
 Vue.config.productionTip = false
 new Vue({
